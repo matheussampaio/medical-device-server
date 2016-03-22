c04962e97f985e69db522558c51f54771c0a64c5
Matheus Sampaio <matheus1401@gmail.com>
2016-03-22 01:26:42 -0500

    fix: syntax error

--- models/outdata.js
+++ models/outdata.js
@@ -2,35 +2,35 @@ var restful = require('node-restful');
 var mongoose = restful.mongoose;
 
 var OUTData = restful.model('outdata', new mongoose.Schema({
-    date: {
-      type: String,
-      trim: true
-    },
-    time: {
-      type: String,
-      trim: true
-    },
-    glucose: {
-      type: String,
-      trim: true
-    },
-    serial: {
-      type: String,
-      trim: true
-    },
-    unit: {
-      type: String,
-      trim: true
-    },
-    userFlag: {
-      type: String,
-      trim: true
-    },
-    mealComment: {
-      type: String,
-      trim: true
-    },
-  })
-  .methods(['get', 'post', 'put', 'delete']);
+  date: {
+    type: String,
+    trim: true
+  },
+  time: {
+    type: String,
+    trim: true
+  },
+  glucose: {
+    type: String,
+    trim: true
+  },
+  serial: {
+    type: String,
+    trim: true
+  },
+  unit: {
+    type: String,
+    trim: true
+  },
+  userFlag: {
+    type: String,
+    trim: true
+  },
+  mealComment: {
+    type: String,
+    trim: true
+  }
+}))
+.methods(['get', 'post', 'put', 'delete']);
 
 module.exports = OUTData;
